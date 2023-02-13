d5f973f352dd70f3a870de0e37854980a58f903d
boluwatifeh <tifeasypeasy@gmail.com>
2023-02-14 00:10:19 +0100

    latest working update

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import {Router, Request, Response} from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -8,10 +9,12 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   const app = express();
 
   // Set the network port
-  const port = process.env.PORT || 8082;
+  const port = process.env.PORT || 8080;
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
+
+  
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
@@ -30,6 +33,21 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+
+  app.get('/filteredimage', async (req: Request, res: Response) => {
+    const image_url = req.query.image_url.toString();
+    if (!image_url) {
+      res.status(400).send('Image url is required');
+    }
+
+    const filtered_image = await filterImageFromURL(image_url);
+
+    res.status(200).sendFile(filtered_image, () => {
+      deleteLocalFiles([filtered_image]);
+    });
+
+    
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
